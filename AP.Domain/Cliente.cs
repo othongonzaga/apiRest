5427b4d4e83b90d72e608b300f2faae506c3a644
othongonzaga <othoncesar1@hotmail.com>
2022-07-29 17:19:46 -0300

    Mudanças no Cliente.cs e no AR.Apresentacao.csproj

--- AP.Domain/Cliente.cs
+++ AP.Domain/Cliente.cs
@@ -1,7 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AP.Domain
 {
+    [Table("Cliente")]
     public class Cliente
     {
         [Key]
