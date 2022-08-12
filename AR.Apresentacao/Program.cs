412853ed61fed30d24781c08e271f2c0dde44017
othongonzaga <othoncesar1@hotmail.com>
2022-08-12 17:04:34 -0300

    Mudanças nos métodos AddSwagger e UseSwaggerUI

--- AR.Apresentacao/Program.cs
+++ AR.Apresentacao/Program.cs
@@ -3,6 +3,7 @@ using AR.Data.Imp;
 using AR.Data.Interfaces;
 using FluentAssertions.Common;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +12,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1",new OpenApiInfo { Title = "ApiRestCliente AR.Apresentacao", Version = "v1"});
+});
 
 builder.Services.AddScoped<IClienteRepository, ClienteRepository>();
 
@@ -24,7 +28,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "APiRestCliente AR.Apresentacao v1"));
 }
 
 app.UseHttpsRedirection();
